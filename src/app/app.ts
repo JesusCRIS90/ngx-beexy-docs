9610faf3defd423ce44aad4af1a266636ec876a5
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-11-12 18:24:26 +0100

    [MOD] Update App

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BeeSidebarLayout } from "./new-beexy-components/bee-sidebar-layout/bee-sidebar-layout"
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BeeSidebarLayout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
