8a845107baa89dcd8dd584a90ce7143a2a8506cf
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-11-17 18:01:05 +0100

    [MOD] Testing Modification of Default Style for BeeSidebarLayout Component.

--- src/app/pages/testing-page/testing-page.ts
+++ src/app/pages/testing-page/testing-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/bee-sidebar-layout';
 
 @Component({
   selector: 'app-testing-page',
-  imports: [],
+  imports: [BeeSidebarLayout],
   templateUrl: './testing-page.html',
   styleUrl: './testing-page.css',
 })
