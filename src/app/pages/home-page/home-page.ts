b09d3648da5a575ff23249fd0fc4a1f8a5137efc
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-11-17 17:36:30 +0100

    [ADD] Basic Pages: home, error and testing have been added.

---
+++ src/app/pages/home-page/home-page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/bee-sidebar-layout';
+
+@Component({
+  selector: 'app-home-page',
+  imports: [BeeSidebarLayout],
+  templateUrl: './home-page.html',
+  styleUrl: './home-page.css',
+})
+export default class HomePage {
+
+}
