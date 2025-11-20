bca58f297ee237c92bb05c53768fd050efbb1e28
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-11-20 19:45:22 +0100

    [MOD] Update testing new functionalities

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
 import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/bee-sidebar-layout';
 
@@ -10,4 +10,28 @@ import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/
 })
 export default class HomePage {
 
+  // ✔ Reference to the child component
+  @ViewChild(BeeSidebarLayout)
+  beeSidebarLayoutRef!: BeeSidebarLayout;
+
+  // ✔ Getter checks real component state
+  public IsSideBarCollapse(): boolean {
+    return this.beeSidebarLayoutRef?.IsCollapsed ?? false;
+  }
+
+  // ✔ Toggle collapse on click
+  ClickCollapse() {
+    console.log("Button Collapse Click");
+
+    if (!this.beeSidebarLayoutRef) return;
+
+    const isCollapse: boolean = this.beeSidebarLayoutRef.IsCollapsed;
+
+    this.beeSidebarLayoutRef.setCollapse( !isCollapse );
+  }
+
+  // ✔ Dynamic button text
+  CollapseButtonText(): string {
+    return this.IsSideBarCollapse() ? "Expand" : "Collapse";
+  }
 }
