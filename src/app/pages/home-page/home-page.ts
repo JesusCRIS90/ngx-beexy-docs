57db0d1f6eb6cad985e681c8492c3e9acaa3e5cb
JesusCRIS90 <Jesus_CR-90@HOTMAIL.ES>
2025-11-20 20:33:37 +0100

    [MOD] Update Test page

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 
-import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/bee-sidebar-layout';
+import { BeeSidebarLayout, SidebarLayoutState } from '../../new-beexy-components/bee-sidebar-layout/bee-sidebar-layout';
 
 @Component({
   selector: 'app-home-page',
@@ -9,7 +9,6 @@ import { BeeSidebarLayout } from '../../new-beexy-components/bee-sidebar-layout/
   styleUrl: './home-page.css',
 })
 export default class HomePage {
-
   // ✔ Reference to the child component
   @ViewChild(BeeSidebarLayout)
   beeSidebarLayoutRef!: BeeSidebarLayout;
@@ -21,17 +20,21 @@ export default class HomePage {
 
   // ✔ Toggle collapse on click
   ClickCollapse() {
-    console.log("Button Collapse Click");
+    // console.log('Button Collapse Click');
 
     if (!this.beeSidebarLayoutRef) return;
 
     const isCollapse: boolean = this.beeSidebarLayoutRef.IsCollapsed;
 
-    this.beeSidebarLayoutRef.setCollapse( !isCollapse );
+    this.beeSidebarLayoutRef.setCollapse(!isCollapse);
   }
 
   // ✔ Dynamic button text
   CollapseButtonText(): string {
-    return this.IsSideBarCollapse() ? "Expand" : "Collapse";
+    return this.IsSideBarCollapse() ? 'Expand' : 'Collapse';
+  }
+
+  onSidebarChange(state: SidebarLayoutState) {
+    console.log('Sidebar updated:', {state});
   }
 }
